30d01ed8cd637673bedebc166f5f7d4c44b84ecb
christycrt <christycrt@gmail.com>
2021-02-05 20:55:52 +0700

    edit vm

--- main.tf
+++ main.tf
@@ -30,8 +30,30 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    security_rule {
+        name                       = "Port_8080"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "8080"
+        destination_port_range     = "*"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+  name                = "INT493-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
 
@@ -42,12 +64,31 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id      = azurerm_network_interface.example.id
+    network_security_group_id = azurerm_network_security_group.example.id
+}
+
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "INT493-machine"
   resource_group_name = var.resource_group_name
   location            = var.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
+  admin_password      = "adminpassword"
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
