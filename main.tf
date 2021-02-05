8826349a749c3f6af4d408f193936c91c51a653e
christycrt <christycrt@gmail.com>
2021-02-05 20:31:10 +0700

    create vm

--- main.tf
+++ main.tf
@@ -16,7 +16,38 @@ provider "azurerm" {
   tenant_id       = "79845616-9df0-43e0-8842-e300feb2642a"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFRG"
-  location = "westus2"
+resource "azurerm_virtual_network" "example" {
+  name                = "INT493-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.resource.location
+  resource_group_name = var.resource.resource_group_name
 }
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = var.resource.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = var.resource.location
+  resource_group_name = var.resource.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "INT493-machine"
+  resource_group_name = var.resource.resource_group_name
+  location            = var.resource.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "CLIENT_SECRET" {
   type = string
   default = "qXCnjYwIaWq9dYOrhJED2fCtfL~c34x7vh"
 }
+
+variable "resource" {
+    type = "map"
+
+    default = {
+        resource_group_name = "INT493"
+        location = "southeastasia"
+    }
+}
