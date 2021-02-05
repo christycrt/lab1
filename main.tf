926ac913dc12b5236146b3df869c071a61c65fe2
christycrt <christycrt@gmail.com>
2021-02-06 03:14:50 +0700

    edit

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 
 resource "azurerm_virtual_network" "example" {
   name                = "INT493-network"
-  address_space       = ["10.0.0.0/24"]
+  address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
 }
