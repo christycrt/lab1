7099ad9871a574fcf90104b778062628c8460ad4
christycrt <christycrt@gmail.com>
2021-02-05 19:29:28 +0700

    add feature

--- main.tf
+++ main.tf
@@ -8,7 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    subscription_id = "b9b38da4-8212-4863-985c-15886d9e8af0"
+    client_id       = "0b1450b4-51ab-4d3e-a632-9ef59a0b5d61"
+    client_secret   = qXCnjYwIaWq9dYOrhJED2fCtfL~c34x7vh
+    tenant_id       = "79845616-9df0-43e0-8842-e300feb2642a"
+  }
 }
 
 resource "azurerm_resource_group" "rg" {
