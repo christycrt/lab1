8cc1a995e63b8ce2c09bc286dbdcf234c8fe76b5
christycrt <christycrt@gmail.com>
2021-02-06 07:56:21 +0700

    add port

--- main.tf
+++ main.tf
@@ -53,6 +53,17 @@ resource "azurerm_network_security_group" "example" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
+    security_rule {
+        name                       = "SSH"
+        priority                   = 300
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "22"
+        destination_port_range     = "*"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 }
 
 resource "azurerm_network_interface" "example" {
